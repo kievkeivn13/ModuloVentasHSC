18e66226c129ebd8e7697e8e44ff1d6f2afd28bf
IngDannyGianca <87837006+IngDannyGianca@users.noreply.github.com>
2021-10-15 15:37:16 -0600

    Update frmMantenimientoTipoInventario.cs
    Tipo Inv Danny S

--- ModuloVentas/CapaVista/frmMantenimientoTipoInventario.cs
+++ ModuloVentas/CapaVista/frmMantenimientoTipoInventario.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaControladorMVentasCC;
 
 namespace CapaVista
 {
 	public partial class frmMantenimientoTipoInventario : Form
 	{
+		Controlador cn = new Controlador();
 		public frmMantenimientoTipoInventario()
 		{
 			InitializeComponent();
+			CenterToScreen();
+			INVMostarTipoInventarios();
 		}
-	}
+
+		public void INVMostarTipoInventarios()
+		{
+			DataTable dt = cn.INVMostarTipoInventarios();
+			dtgTipoInventario.DataSource = dt;
+		}
+
+
+		private void btnIngresar_Click(object sender, EventArgs e)
+        {
+			string Pkid = txtidtinventario.Text;
+			string Nombre = txtnombretinventario.Text;
+			string Estatus = "0";
+			if (rbnHabilitado.Checked == true)
+			{
+				rbnHabilitado.Checked = true;
+				Estatus = "1";
+
+			}
+			else if (rbnInhabilitado.Checked == true)
+			{
+				rbnInhabilitado.Checked = true;
+				Estatus = "0";
+
+			}
+			cn.INVInsertarTipoInventarios(Pkid, Nombre, Estatus);
+
+			INVMostarTipoInventarios();
+		}
+
+        private void dtgTipoInventario_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+			txtidtinventario.Text = dtgTipoInventario.CurrentRow.Cells[0].Value.ToString();
+		}
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+			txtidtinventario.Text="";
+			txtnombretinventario.Text="";
+			rbnHabilitado.Checked = true;
+			rbnInhabilitado.Checked = false;
+
+		}
+    }
 }
