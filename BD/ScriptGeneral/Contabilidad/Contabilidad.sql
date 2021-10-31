4454e8f43505aaae0ea30c04f22d154830e202d9
Brayan-Cifuentes <brayancifuentes33@gmail.com>
2021-10-31 14:51:59 -0600

    Depuracion Base de Datos General unida

--- BD/ScriptGeneral/Contabilidad/Contabilidad.sql
+++ BD/ScriptGeneral/Contabilidad/Contabilidad.sql
@@ -5,17 +5,15 @@ use hotelSanCarlos;
 
 -- Tabla de históricos donde se almacenará la información al hacer un cierre
 
-
-
 -- ------------------------------------------------------------------------------------------------------------
 -- 							MÓDULO CONTABILIDAD - CIERRE CONTABLE
 -- ------------------------------------------------------------------------------------------------------------
 create table guardarReportecierre(
-idReporte varchar(15),
-rutaReporte varchar (100),
-estado varchar(1),
+	idReporte varchar(15),
+	rutaReporte varchar (100),
+	estado varchar(1),
 
-primary key (idReporte)
+	primary key (idReporte)
 ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 -- Tabla de históricos donde se almacenará la información al hacer un cierre
@@ -40,76 +38,73 @@ create table historico(
 ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 create table balanceSaldos(
-idBalance varchar(15),
-nombre varchar(20),
-sumas float,
-saldos float,
-estado varchar(1),
+	idBalance varchar(15),
+	nombre varchar(20),
+	sumas float,
+	saldos float,
+	estado varchar(1),
 
-primary key (idBalance)
+	primary key (idBalance)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 create table ajusteContable(
-idAjusteContable varchar(15),
-descripcion varchar(20),
-estado varchar(1),
+	idAjusteContable varchar(15),
+	descripcion varchar(20),
+	estado varchar(1),
 
 primary key (idAjusteContable)
-);
+)ENGINE = InnoDB DEFAULT CHARSET=latin1;
 -- ------------------------------------------------------------------------------------------------------------
 -- 							MÓDULO CONTABILIDAD - PRESUPUESTOS
 -- ------------------------------------------------------------------------------------------------------------
 
 create table presupuesto(
-idPresupuesto varchar(15),
-nomPresupuesto varchar(20),
-fechaPresupuesto date,
-ingresoPresupuesto int,
-gastoPresupuesto int,
-totalPresupuesto int,
-
-
-primary key (idPresupuesto)
+	idPresupuesto varchar(15),
+	nomPresupuesto varchar(20),
+	fechaPresupuesto date,
+	ingresoPresupuesto int,
+	gastoPresupuesto int,
+	totalPresupuesto int,
+    
+	primary key (idPresupuesto)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
 -- ------------------------------------------------------------------------------------------------------------
 -- 							MÓDULO CONTABILIDAD - ESTADOS FINANCIEROS
 -- ------------------------------------------------------------------------------------------------------------
+create table cuentaEstado(
+	numCuenta varchar(15),
+	nombreEstado varchar(20),
+	saldo float,
+	fecha date,
 
-create table CuentaEstado(
-numCuenta varchar(15),
-nombreEstado varchar(20),
-saldo float,
-fecha date,
-
-primary key (numCuenta)
+	primary key (numCuenta)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 create table reporteEstado(
-idReporteEstado varchar(15),
-rutaEstado varchar(20),
+	idReporteEstado varchar(15),
+	rutaEstado varchar(20),
 
-primary key (idReporteEstado)
-);
-
-create table EstadoIngreso(
-idEstadoIngreso varchar(15),
-fechaIngreso date,
-saldoAcumulado float,
-estado varchar(1),
-
-primary key (idEstadoIngreso)
+	primary key (idReporteEstado)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
-create table EstadoSaldo(
-idEstadoSaldo varchar(15),
-fechaSaldo date,
-estado varchar(1),
+create table estadoIngreso(
+	idEstadoIngreso varchar(15),
+	fechaIngreso date,
+	saldoAcumulado float,
+	estado varchar(1),
 
-primary key (idEstadoSaldo)
+	primary key (idEstadoIngreso)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
+create table estadoSaldo(
+	idEstadoSaldo varchar(15),
+	fechaSaldo date,
+	estado varchar(1),
+
+	primary key (idEstadoSaldo)
+)ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
 -- ------------------------------------------------------------------------------------------------------------
@@ -117,33 +112,32 @@ primary key (idEstadoSaldo)
 -- ------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE activosFijos(
-idActivo varchar(15),
-nombreActivo varchar(100),
-tipoActivo varchar (100),
-descripcion varchar (100),
-
-
-primary key (idActivo)
+	idActivo varchar(15),
+	nombreActivo varchar(100),
+	tipoActivo varchar (100),
+	descripcion varchar (100),
+    
+	primary key (idActivo)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE depreciacionActivo(
-idDepreciacion varchar (15),
-idActivo varchar(50),
-fechaAdquisicion date,
-costoActvo decimal (10,0),
-vidaUtil int,
-mesesUso int,
-cuotaDepreciacion decimal (10,0),
-depreciacionAcumulada decimal (10,0),
-valorNeto decimal (10,0),
-fechaDepreciacion date,
-primary key (idDepreciacion)
+	idDepreciacion varchar (15),
+	idActivo varchar(50),
+	fechaAdquisicion date,
+	costoActvo decimal (10,0),
+	vidaUtil int,
+	mesesUso int,
+	cuotaDepreciacion decimal (10,0),
+	depreciacionAcumulada decimal (10,0),
+	valorNeto decimal (10,0),
+	fechaDepreciacion date,
+	primary key (idDepreciacion)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE tipoActivo(
-idTipoactivo varchar(15),
-tipo varchar (100),
-estado varchar(1),
-
-primary key (idTipoactivo)
+	idTipoactivo varchar(15),
+	tipo varchar (100),
+	estado varchar(1),
+	
+	primary key (idTipoactivo)
 )ENGINE = InnoDB DEFAULT CHARSET=latin1;
--- BD/ScriptGeneral/RRHH/RRHH.sql
+++ BD/ScriptGeneral/RRHH/RRHH.sql
@@ -1,65 +1,87 @@
 USE hotelSanCarlos;
-
+-- ------------------------------------------------------------------------------------------------------------
+-- 							MÓDULO RRHH - PÓLIZAS
+-- ------------------------------------------------------------------------------------------------------------
 CREATE TABLE prestacionlaboral (
-  pkIdPrestacionLaboral VARCHAR(11) PRIMARY KEY,
-  nombre VARCHAR(45) NULL DEFAULT NULL,
-  descripcion VARCHAR(45) NULL DEFAULT NULL)
+	pkIdPrestacionLaboral VARCHAR(11) PRIMARY KEY,
+	nombre VARCHAR(45) NULL DEFAULT NULL,
+	calcularDesde int NULL DEFAULT NULL,
+	descripcion VARCHAR(45) NULL DEFAULT NULL)
  ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 
 CREATE TABLE totalPrestacion (
-  pkPrestacion VARCHAR(11) PRIMARY KEY,
-  fkIdEmpleado VARCHAR(11) NOT NULL,
-  fkIdPrestacionLaboral VARCHAR(11) NOT NULL,
-  fechaInicio DATETIME NULL,
-  fechaFInal DATETIME NULL,
-  totalDiasTrabajados INT NULL,
-  totalPrestacion DECIMAL NULL,
+	pkPrestacion VARCHAR(11) PRIMARY KEY,
+	fkIdEmpleado VARCHAR(11) NOT NULL,
+	fkIdPrestacionLaboral VARCHAR(11) NOT NULL,
+	fechaInicio DATETIME NULL,
+	fechaFInal DATETIME NULL,
+	totalDiasTrabajados INT NULL,
+	totalPrestacion DECIMAL NULL,
   
-  FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado),
-  FOREIGN KEY (fkIdPrestacionLaboral) REFERENCES prestacionlaboral (pkIdPrestacionLaboral))
+	FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado),
+	FOREIGN KEY (fkIdPrestacionLaboral) REFERENCES prestacionlaboral (pkIdPrestacionLaboral))
  ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE totalLiquidacion (
-  fkIdPrestacion VARCHAR(11) NOT NULL,
-  totalLiquidacion INT NULL,
-  
-  FOREIGN KEY (fkIdPrestacion) REFERENCES totalPrestacion (pkPrestacion))
- ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 CREATE TABLE desarrollodecarrrera (
-  pkIdDesarrolloDeCarrera VARCHAR(11) PRIMARY KEY,
-  observaciones VARCHAR(45) NULL DEFAULT NULL,
-  aspiradorSueldo VARCHAR(45) NULL DEFAULT NULL,
-  ausencias VARCHAR(45) NULL DEFAULT NULL,
-  fkIdEmpleado VARCHAR(11) NOT NULL,
+	pkIdDesarrolloDeCarrera VARCHAR(11) PRIMARY KEY,
+	observaciones VARCHAR(45) NULL DEFAULT NULL,
+	aspiradorSueldo VARCHAR(45) NULL DEFAULT NULL,
+	ausencias VARCHAR(45) NULL DEFAULT NULL,
+	fkIdEmpleado VARCHAR(11) NOT NULL,
   
-  FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado))
+	FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado))
  ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 
 CREATE TABLE mediodecontacto (
-  pkIdMedioDeContacto VARCHAR(11) PRIMARY KEY,
-  nombre VARCHAR(45) NULL DEFAULT NULL,
-  costoMensual VARCHAR(45) NULL DEFAULT NULL,
-  mediodecontactocol VARCHAR(45) NULL DEFAULT NULL
-  )
- ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
+	pkIdMedioDeContacto VARCHAR(11) PRIMARY KEY,
+	nombre VARCHAR(45) NULL DEFAULT NULL,
+	costoMensual VARCHAR(45) NULL DEFAULT NULL,
+	mediodecontactocol VARCHAR(45) NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE reclutamiento (
-  pkIdReclutamiento VARCHAR(11) PRIMARY KEY,
-  fkIdEmpleado VARCHAR(11) NOT NULL,
-  fkIdMedioDeContacto VARCHAR(11) NOT NULL,
+	pkIdReclutamiento VARCHAR(11) PRIMARY KEY,
+	fkIdEmpleado VARCHAR(11) NOT NULL,
+	fkIdMedioDeContacto VARCHAR(11) NOT NULL,
+	
+	FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado),
+	FOREIGN KEY (fkIdMedioDeContacto) REFERENCES mediodecontacto (pkIdMedioDeContacto)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE diasLaborados (
+	pkDiasTrabajados VARCHAR(11) PRIMARY KEY,
+	fkIdEmpleado VARCHAR(11) NOT NULL,
+	año INT NULL,
+	enero INT NULL,
+	febrero INT NULL,
+	marzo INT NULL,
+	abril INT NULL,
+	mayo INT NULL,
+	junio INT NULL,
+	julio INT NULL,
+	agosto INT NULL,
+	septiembre INT NULL,
+	octubre INT NULL,
+	noviembre INT NULL,
+	diciembre INT NULL,
+	totalDiasTrabajados INT NULL,
   
-  FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado),
-  FOREIGN KEY (fkIdMedioDeContacto) REFERENCES mediodecontacto (pkIdMedioDeContacto))
- ENGINE=InnoDB DEFAULT CHARSET=latin1;
+	FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE nominas (
+	pkIdNominas VARCHAR(11) PRIMARY KEY, 
+	fkIdEmpleado VARCHAR(11) NOT NULL,
+	año INT NULL,
+	mes VARCHAR(45) NULL,
+	diasLaborados VARCHAR(45) NULL,
+	horasOrdinarias VARCHAR(45) NULL,
+	horasExtraordinarias VARCHAR(45) NULL,
+	salarioTotal VARCHAR(45) NULL,
+	igss VARCHAR(45) NULL,
+	isr VARCHAR(45) NULL,
+	totalDeducciones VARCHAR(45) NULL,
+	salarioLiquido VARCHAR(45) NULL,
 
+	FOREIGN KEY (fkIdEmpleado) REFERENCES empleado (pkIdEmpleado)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
